413682796e13172942a352ac428e6008d3378faa
MendezBonito, Ignacio <MendezBonito.Ignacio@bcg.com>
2024-10-27 14:56:25 +0100

    env variable for domains

--- api/verificar-captcha.js
+++ api/verificar-captcha.js
@@ -1,59 +1,49 @@
-import express from 'express';
-import cors from 'cors';
 import { RecaptchaEnterpriseServiceClient } from '@google-cloud/recaptcha-enterprise';
-require('dotenv').config();
 
-const app = express();
 const client = new RecaptchaEnterpriseServiceClient();
-const projectID = "my-project-49814-1729980997678";
-const recaptchaKey = "6LfHHG0qAAAAAH8ZER3UVuDuCxyDr5OgoYO480cE";
+const projectID = "my-project-49814-1729980997678"; // Tu Project ID de Google Cloud
+const recaptchaKey = "6LfHHG0qAAAAAH8ZER3UVuDuCxyDr5OgoYO480cE"; // Tu Site Key de reCAPTCHA
 
-app.use(express.json());
-app.use(cors());
-// Permitir CORS para el dominio de tu frontend
-const allowedOrigins = ['https://www.hipotecasinentradas.com']; // Asegúrate de agregar tu dominio aquí
-app.use(cors({ origin: allowedOrigins }));
-
-// Ruta para verificar el token de reCAPTCHA
-app.post('/api/verificar-captcha', async (req, res) => {
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
     const token = req.body.token;
-console.log("entro por aqui")
+
     try {
-        const projectPath = client.projectPath(projectID);
-        const request = {
-            assessment: {
-                event: {
-                    token: token,
-                    siteKey: recaptchaKey,
-                },
-            },
-            parent: projectPath,
-        };
+      const projectPath = client.projectPath(projectID);
+      const request = {
+        assessment: {
+          event: {
+            token: token,
+            siteKey: recaptchaKey,
+          },
+        },
+        parent: projectPath,
+      };
 
-        const [response] = await client.createAssessment(request);
+      // Llamada a la API de Google reCAPTCHA Enterprise
+      const [response] = await client.createAssessment(request);
 
-        if (!response.tokenProperties.valid) {
-            return res.status(400).json({ success: false, message: `Token inválido: ${response.tokenProperties.invalidReason}` });
-        }
+      if (!response.tokenProperties.valid) {
+        return res.status(400).json({ success: false, message: `Token inválido: ${response.tokenProperties.invalidReason}` });
+      }
 
-        const recaptchaAction = "submit";
-        if (response.tokenProperties.action !== recaptchaAction) {
-            return res.status(400).json({ success: false, message: 'La acción no coincide' });
-        }
+      const recaptchaAction = "submit"; // Asegúrate de que esto coincida con la acción en el frontend
+      if (response.tokenProperties.action !== recaptchaAction) {
+        return res.status(400).json({ success: false, message: 'La acción no coincide' });
+      }
 
-        const score = response.riskAnalysis.score;
-        if (score > 0.5) {
-            return res.json({ success: true, message: 'Verificación de CAPTCHA exitosa', score: score });
-        } else {
-            return res.status(400).json({ success: false, message: 'Posible bot', score: score });
-        }
+      const score = response.riskAnalysis.score;
+      if (score > 0.5) {
+        return res.json({ success: true, message: 'Verificación de CAPTCHA exitosa', score: score });
+      } else {
+        return res.status(400).json({ success: false, message: 'Posible bot', score: score });
+      }
     } catch (error) {
-        console.error('Error al verificar CAPTCHA:', error);
-        res.status(500).json({ success: false, message: 'Error interno del servidor' });
+      console.error('Error al verificar CAPTCHA:', error);
+      res.status(500).json({ success: false, message: 'Error interno del servidor' });
     }
-});
-
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-    console.log(`Servidor backend escuchando en http://localhost:${port}`);
-});
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
