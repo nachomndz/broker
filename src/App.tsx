466bd4d46d096957f7bce9f9303432a232ca7d32
MendezBonito, Ignacio <MendezBonito.Ignacio@bcg.com>
2024-10-27 02:04:03 +0200

    feature: including global file

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Services from './components/Services';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import './App.css';
+
 function App() {
   return (
     <div className="App">
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,3 @@
-// import React from 'react';
 
 import  { useState, useEffect } from 'react';
 import { useForm, ValidationError } from '@formspree/react';
